02c48dd140aed129072bd5e74524321bc9ab0b0d
Pranjal sharma <onlinemarketingclasses247@gmail.com>
2026-04-02 18:09:03 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,9 @@ export default {
   theme: {
     extend: {
       colors: {
-        border: "hsl(214.3 31.8% 91.4%)",
-        background: "hsl(0 0% 100%)",
-        foreground: "hsl(222.2 84% 4.9%)",
-        primary: "hsl(222.2 47.4% 11.2%)",
+        primary: "#000000",
+        secondary: "#ffffff",
+        accent: "#2563eb",
       },
     },
   },
