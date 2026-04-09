d5f82c7ad215b982e58437ca4bb25c523b8d3370
Pranjal sharma <onlinemarketingclasses247@gmail.com>
2026-04-10 04:56:40 +0530

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -222,12 +222,11 @@ export default function Contact() {
                   method="POST"
                   className="space-y-5"
                 >
-                  <input type="hidden" name="_captcha" value="false" />
-                  <input type="hidden" name="_subject" value="New Lead from Website" />
-                  <input type="hidden" name="_template" value="table" />
-                  <input type="hidden" name="_honey" style={{ display: "none" }} />
-                   <input type="hidden" name="_template" value="table" />
-                   <input type="hidden" name="_captcha" value="false" />  
+                <input type="hidden" name="_captcha" value="false" />
+<input type="hidden" name="_subject" value="New Lead from Website" />
+<input type="hidden" name="_template" value="table" />
+<input type="hidden" name="_next" value="https://pranjaldigital.com/thank-you" />
+<input type="hidden" name="_honey" style={{ display: "none" }} />
 
                   <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
                     <div>
