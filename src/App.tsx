5fafe993865ba9e5e20ca1b18f4c8766351ebba5
Pranjal sharma <onlinemarketingclasses247@gmail.com>
2026-04-22 03:08:49 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import About from "@/pages/About";
 import Contact from "@/pages/Contact";
 import FreeGoogleAdsCompetitorResearch from "./pages/ai-products/FreeGoogleAdsCompetitorResearch";
 import AIProducts from "./pages/AIProducts";
-
+import AiVoiceAgentDentist from "@/pages/AiVoiceAgentDentist";
 
 import CaseStudies from "@/pages/CaseStudies";
 import TermsOfService from "@/pages/TermsOfService";
@@ -30,7 +30,7 @@ function Router() {
         <Route path="/contact" component={Contact} />
         <Route path="/thank-you" component={ThankYou} />
         <Route path="/ai-products" component={AIProducts} />
-        <Route path="/ai-products" component={AIProducts} />
+        <Route path="/ai-voice-agent-dentist" component={AiVoiceAgentDentist} />
       
         <Route path="/case-studies" component={CaseStudies} />
   <Route path="/terms-of-service" component={TermsOfService} />
